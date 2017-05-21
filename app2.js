0cf9af3e89590eae6c6800e870e9e6693496a0af
HurricanePete <zpete725@gmail.com>
2017-05-21 19:08:23 -0400

    Ultimate Commit - fix correct functionaliy for after-answer screen

--- app2.js
+++ app2.js
@@ -1,12 +1,13 @@
 var state = {
 	current: 1,
-	score: 0
+	score: 0,
+	screen: 'questions'
 }
 
 var questions = {
 	items: [
 	question1 = {
-		question: '"When Indiana Jones first looks into the Well of the Souls, what does he say about the animals he sees on the floor?"',
+		question: 'When Indiana Jones first looks into the Well of the Souls, what does he say about the animals he sees on the floor?',
 		answerOne: '"Dogs! I friggin\' love dogs!"',
 		answerTwo: '"Huh. I didn\'t know they had beavers in Egypt."',
 		answerThree: "There were no animals in the Well of the Souls",
@@ -45,59 +46,50 @@ var questions = {
 
 function correct (state) {
 	(state.score)++;
-  console.log("state.score equals " + state.score);
 }
 
 function current (state) {
 	(state.current)++;
-  console.log("state.current equals " + state.current);
+}
+
+function reset (state) {
+	state.current = 1,
+	state.score = 0,
+	state.screen = 'questions'
+}
+
+function screen (state) {
+	state.screen = 'questions';
+}
+
+function otherScreen (state) {
+	state.screen = 'status';
 }
 
 function checkItem (state, questions, targetItem, element) {
 		if (question1.correct === targetItem) {
 			correct(state);
-			//element.html('<button class="answers correct">' + '<span>' + question1.correct + '</span>' + '</button>');
 		}
 		else if (question2.correct === targetItem) {
 			correct(state);
-			//element.html('<button class="answers correct">' + '<span>' + question2.correct + '</span>' + '</button>');
 		}
 		else if (question3.correct === targetItem) {
 			correct(state);
-			//element.html('<button class="answers correct">' + '<span>' + question3.correct + '</span>' + '</button>');
 		}
 		else if (question4.correct === targetItem) {
 			correct(state);
-			//element.html('<button class="answers correct">' + '<span>' + question4.correct + '</span>' + '</button>');
 		}
 		else if (question5.correct === targetItem) {
 			correct(state);
-			//element.html('<button class="answers correct">' + '<span>' + question5.correct + '</span>' + '</button>');
 		}
-		alert('Correct!');
 	}
-
-function wrongItem (state, questions) {
-	if (state.current === 1) {
-		alert('That is incorrect, the correct answer is ' + question1.correct);
-	}
-	else if (state.current === 2) {
-		alert('That is incorrect, the correct answer is ' + question2.correct);
-	}
-	else if (state.current === 3) {
-		alert('That is incorrect, the correct answer is ' + question3.correct);
-	}
-	else if (state.current === 4) {
-		alert('That is incorrect, the correct answer is ' + question4.correct);
-	}
-	else if (state.current === 5) {
-		alert('That is incorrect, the correct answer is ' + question5.correct);
-	}
-}
 
 function questionRender (state, questions, element) {
 	var questionItem = function() {
-		if (state.current === 2) {
+		if (state.current === 1) {
+			return '<h2>Question 1</h2>' + '<h3>' + question1.question + '</h3>';
+		}
+		else if (state.current === 2) {
 			return '<h2>Question 2</h2>' + '<h3>' + question2.question + '</h3>';
 		}
 		else if (state.current === 3) {
@@ -118,7 +110,13 @@ function questionRender (state, questions, element) {
 
 function answerRender (state, questions, element, element2) {
 	var answerItem = function () {
-		if (state.current === 2) {
+		if (state.current === 1) {
+			return '<button class="answers wrong">' + '<span>' + question1.answerOne + '</span>' + '</button>' +
+				'<button class="answers wrong">' + '<span>' + question1.answerTwo + '</span>' + '</button>' +
+				'<button class="answers rightAnswer">' + '<span>' + question1.correct + '</span>' + '</button>' +
+				'<button class="answers wrong">' + '<span>' + question1.answerThree + '</span>' + '</button>';
+		}
+		else if (state.current === 2) {
 			return '<button class="answers wrong">' + '<span>' + question2.answerOne + '</span>' + '</button>' +
 				'<button class="answers wrong">' + '<span>' + question2.answerTwo + '</span>' + '</button>' +
 				'<button class="answers rightAnswer">' + '<span>' + question2.correct + '</span>' + '</button>' +
@@ -151,7 +149,10 @@ function answerRender (state, questions, element, element2) {
 		else if (state.current === 6 && state.score > 1) {
 			return '<span><h2 class="page">It was close, but you survived. Your score is ' + state.score + ' out of 5</h2></span><hr>' + '<div class="page">' + '<img class="page score-image" src="boulder.jpg">' + '</div>'; 
 		}
-		else if (state.current === 6 && state.score <= 1) {
+		else if (state.current === 6 && state.score < 1) {
+			return '<span><h2 class="page">Catastrophe! Your score is ' + state.score + ' out of 5. Give it another try!</h2></span><hr>' + '<div class="page">' + '<img class="page score-image" src="low-score.jpg">' + '</div>'; 
+		}
+		else if (state.current === 6 && state.score === 0) {
 			return '<span><h2 class="page">Catastrophe! Your score is ' + state.score + ' out of 5. Give it another try!</h2></span><hr>' + '<div class="page">' + '<img class="page score-image" src="low-score.jpg">' + '</div>'; 
 		}
 	};
@@ -165,36 +166,91 @@ function scoringRender (state, element) {
 
 function endGame (state, element) {
 	if (state.current > 5) {
-		element.html('<div class="nav-button">' + '<button href="https://hurricanepete.github.io/My-Quiz-App/" class="reset "><span>Start Over</span></button>' + '</div>');
-	};
+		element.html('<div class="nav-button">' + '<button class="reset"><span>Start Over</span></button>' + '</div>');
+	}
+}
+
+function startGame (state, element) {
+	if (state.current === 1) {
+		element.html('<button class="reset"><span>Start Over</span></button>' + '<div class="scoring">' + '<span class="current place">Question 1 of 5</span>' + '<br>' + '<hr>' +
+			'<span class="current score">0 of 5 Correct</span>' + '</div>' + '<button class="next-question"><span>Next Question</span></button>');
+	}
+}
+
+function correctStatusRender (state, questions, target) {
+	target.closest('body').find('.pop-up').toggleClass("hidden");
+	target.closest('body').find('.my-form').toggleClass("hidden");
+	target.closest('body').find('.pop-up').html('<span class="status">' + 'Correct!</span>');
+}
+
+function incorrectStatusRender (stat, questions, target) {
+	target.closest('body').find('.pop-up').toggleClass("hidden");
+	target.closest('body').find('.my-form').toggleClass("hidden");
+	if (state.current === 1) {
+	target.closest('body').find('.pop-up').html(
+		'<span class="status">' + 'Incorrect! The correct answer is ' + question1.correct + '</span>');
+	}
+	else if (state.current === 2) {
+	target.closest('body').find('.pop-up').html(
+		'<span class="status">' + 'Incorrect! The correct answer is ' + question2.correct + '</span>');
+	}
+	else if (state.current === 3) {
+	target.closest('body').find('.pop-up').html(
+		'<span class="status">' + 'Incorrect! The correct answer is ' + question3.correct + '</span>');
+	}
+	else if (state.current === 4) {
+	target.closest('body').find('.pop-up').html(
+		'<span class="status">' + 'Incorrect! The correct answer is ' + question4.correct + '</span>');
+	}
+	else if (state.current === 5) {
+	target.closest('body').find('.pop-up').html(
+		'<span class="status">' + 'Incorrect! The correct answer is ' + question5.correct + '</span>');
+	}
+}
+
+function questionOrAnswer (state, questions, elementQ, elementA, elementS, elementE, target) {
+	if (state.screen === 'questions') {
+		questionRender(state, questions, elementQ);
+		answerRender(state, questions, elementA);
+		scoringRender(state, elementS);
+		endGame(state, elementE);
+	} 
+	else if (state.screen === 'status') {
+		correctStatusRender(state, questions, target);
+	}
+}
+
+function revert (target) {
+	target.closest('body').find('.pop-up').addClass("hidden");
+	target.closest('body').find('.my-form').removeClass("hidden");
 }
 
 $(function() {
 	$('form').on('click', 'button.rightAnswer', function(event) {
 		event.preventDefault();
-		console.log('You clicked me');
 		$(this).toggleClass('clicked');
 		var quizItem = $(this).closest('form').find('.clicked');
+		otherScreen(state);
 		checkItem(state, questions, quizItem.text(), $('.rightAnswer'));
-		scoringRender(state, $('.scoring'));
-		current(state);
-		scoringRender(state, $('.scoring'));
-		questionRender(state, questions, $('.question-title'));
-		answerRender(state, questions, $('.my-form'));
-		endGame(state, $('.navigation'));
+		correctStatusRender(state, questions, $(this));
 	});
 	$('form').on('click', 'button.wrong', function(event) {
-		wrongItem(state, questions);
-		current(state);
-		questionRender(state, questions, $('.question-title'));
-		answerRender(state, questions, $('.my-form'));
-		endGame(state, $('.navigation'));
+		event.preventDefault();
+		otherScreen(state);
+		incorrectStatusRender(state, questions, $(this));
 	});
 	$('div.navigation').on('click', 'button.next-question', function(event) {
+		event.preventDefault();
 		current(state);
-		scoringRender(state, $('.scoring'));
-		questionRender(state, questions, $('.question-title'));
-		answerRender(state, questions, $('.my-form'));
-		endGame(state, $('.navigation'));
+		screen(state);
+		revert($(this));
+		questionOrAnswer(state, questions, $('.question-title'), $('.my-form'), $('.scoring'), $('.navigation'), $(this));
 	});
+	$('div.navigation').on('click', 'button.reset', function(event) {
+		event.preventDefault();
+		reset(state);
+		questionOrAnswer(state, questions, $('.question-title'), $('.my-form'), $('.scoring'), $('.navigation'), $(this));
+		revert($(this));
+		startGame(state, $('.navigation'));
+	})
 });
